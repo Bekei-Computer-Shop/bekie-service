940bdd34dbdf0f52123a219c17f677391d32a4e5
HorRatha <horratha4t5@gmail.com>
2026-07-02 16:10:29 +0700

    fixed the error conflex version

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'first_name',
         'last_name',
         'username',
